27d561ea9558c5150a690c8a71771a27218ceb1b
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2021-02-05 17:08:27 +0530

    mongoose

--- mongoose_1.js
+++ mongoose_1.js
@@ -1,5 +1,24 @@
 const mongoose=require("mongoose");
 
 //connection with mongoDb
-mongoose.connect('mongodb://127.0.0.1:27017/',useNewURLParser:true).
-    then(()=>console.log("Connected")).catch(()=>{console.log("Error")})
+mongoose.connect('mongodb://127.0.0.1:27017/',{useNewUrlParser: true,useUnifiedTopology:true, useFindAndModify: false}).
+    then(()=>console.log("Connectedd")).catch(()=>{console.log("Error")});
+
+const schema1=new mongoose.Schema({
+    name:{type:String, require:true},
+    course:String,
+    date:{type:Date, default:Date.now},
+    reg:Boolean,
+    duration:Number
+});
+
+const db=new mongoose.model("new_db",schema1);
+const db1=new db({
+    name:"Hotesh",
+    course:"kj&^*3h3kj",
+    reg:true,
+    duration:45
+})
+db1.save().then(()=>console.log("saved")).catch(()=>console.log("error_unsaved"));
+
+console.log("end")
