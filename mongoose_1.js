709a4f36a087722dbdd86369f7d5a2dabf8325f1
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2021-02-08 16:08:21 +0530

    moongoose

--- mongoose_1.js
+++ mongoose_1.js
@@ -1,24 +1,35 @@
 const mongoose=require("mongoose");
 
 //connection with mongoDb
-mongoose.connect('mongodb://127.0.0.1:27017/',{useNewUrlParser: true,useUnifiedTopology:true, useFindAndModify: false}).
+mongoose.connect('mongodb://127.0.0.1:27017/new_database',{useNewUrlParser: true,useUnifiedTopology:true, useFindAndModify: false}).
     then(()=>console.log("Connectedd")).catch(()=>{console.log("Error")});
 
 const schema1=new mongoose.Schema({
-    name:{type:String, require:true},
+    name:{type:String, require:[true,"Name field must be required"] },
     course:String,
     date:{type:Date, default:Date.now},
     reg:Boolean,
     duration:Number
 });
 
-const db=new mongoose.model("new_db",schema1);
+const db=new mongoose.model("new_collection",schema1);
 const db1=new db({
     name:"Hotesh",
     course:"kj&^*3h3kj",
     reg:true,
     duration:45
 })
-db1.save().then(()=>console.log("saved")).catch(()=>console.log("error_unsaved"));
+
+const db2=new db({
+    name:"YOgesh",
+    course:"bhakk",
+    reg:false,
+    duration:455
+})
+
+//db1.save().then(()=>console.log("saved")).catch(()=>console.log("error_unsaved"));
+db.insertMany([db1,db2])
+db.findByIdAndUpdate("6021134b87200b07a8535d86",{name:"Yogesh"}).then((doc)=>console.log(doc)).catch((err)=>console.log(err));
+//db.findByIdAndDelete("6021134b87200b07a8535d86");
 
 console.log("end")
